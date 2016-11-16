4ed92a89e0f4d8a12e7f2717d4123288effc912a
DarkJ24 <jose.quesada2464@gmail.com>
2016-11-16 09:30:51 -0600

    added Solicitud de Cambios Controller

---
+++ ControldeCambios/Controllers/Solicitud_CambiosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControldeCambios.Controllers
+{
+    public class Solicitud_CambiosController : ToastrController
+    {
+        // GET: Solicitud_Cambios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
