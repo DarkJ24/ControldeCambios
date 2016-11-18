2e9a5a3c3c2cbe640197e79051e5a0f0761902ca
Juancalmur <juancalmur@hotmail.com>
2016-11-18 00:38:32 -0600

    Índice de solicitud de cambios en progreso

---
+++ ControldeCambios/Models/Solicitud_CambiosIndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControldeCambios.Models
+{
+    public class Solicitud_CambiosIndexModel
+    {
+        public string proyecto { get; set; }
+        public List<Requerimiento> reqs { get; set; }
+        public Boolean crearRequerimientos { get; set; }
+        public Boolean detallesRequerimientos { get; set; }
+    }
+}
